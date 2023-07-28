148bab4ea7c7e6dc200cd30b280f9644641da687
Rohan Magar <rohan.magar.415@gmail.com>
2023-07-28 17:15:31 +0545

    solved merge conflict

--- utils/mailer.js
+++ utils/mailer.js
@@ -6,7 +6,7 @@ const mailer = nodemailer.createTransport(
   {
     host: process.env.NODEMAILER_HOST,
     port: process.env.NODEMAILER_PORT,
-    secure: false, // upgrade later with STARTTLS
+    secure: false,
     auth: {
       user: process.env.NODEMAILER_USERNAME,
       pass: process.env.NODEMAILER_PASSWORD,
